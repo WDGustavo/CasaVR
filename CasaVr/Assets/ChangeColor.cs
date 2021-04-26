622b9d1308ae8a9f84dd9d91ccbb9a4dc3200785
WDGustavo <54276455+WDGustavo@users.noreply.github.com>
2021-04-26 20:08:11 -0300

    primeiro

---
+++ CasaVr/Assets/ChangeColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void Blue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
---
+++ CasaVr/Assets/MovePlayer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+
+    float speed = 10;
+    bool isFoward = false;
+    bool isBack = false;
+    bool isLeft = false;
+    bool isRight = false;
+
+    void Update()
+    {
+        if (isFoward)
+        {
+            transform.Translate(0, 0, (speed *Time.deltaTime));
+        }
+        if (isBack)
+        {
+            transform.Translate(0, 0, (-speed * Time.deltaTime));
+        }
+        if (isLeft)
+        {
+            transform.Translate((-speed * Time.deltaTime), 0, 0);
+        }
+        if (isRight)
+        {
+            transform.Translate((speed * Time.deltaTime), 0, 0);
+        }
+    }
+    void EnableMoveController(bool frente = false,bool tras = false, bool esquerda = false, bool direita = false)
+    {
+        if(frente)
+            isFoward = frente;
+        if (tras)
+            isBack = tras;
+        if (esquerda)
+            isLeft = esquerda;
+        if (direita)
+            isRight = direita;
+    }
+    public void DisableMoveController()
+    {
+        isFoward = false;
+        isBack = false;
+        isLeft = false;
+        isRight = false;
+    }
+
+    public void Foward()
+    {
+        EnableMoveController(true);
+    }
+    public void Back()
+    {
+        EnableMoveController(false,true);
+    }
+    public void Left()
+    {
+        EnableMoveController(false,false,true);
+    }
+    public void Right()
+    {
+        EnableMoveController(false,false,false,true);
+    }
+}
