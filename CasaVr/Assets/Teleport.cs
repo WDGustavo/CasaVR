ac2390d265e42a1cc5d2e4117abfe9d22363cb29
WDGustavo <54276455+WDGustavo@users.noreply.github.com>
2021-05-10 23:42:42 -0300

    MoveisETeleporte

---
+++ CasaVr/Assets/Teleport.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public void TeleportPlayer(Transform pos)
+    {
+        transform.position = pos.position;
+    }
+}
