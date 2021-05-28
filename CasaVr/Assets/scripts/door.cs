3d0a5515b02762b7ae83250f7242de76ae4c5fe8
sigor1202 <sigor7829@gmail.com>
2021-05-27 21:29:33 -0300

    final

---
+++ CasaVr/Assets/scripts/door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    bool BoolDoor;
+    public Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        BoolDoor = anim.GetBool("open");
+    }
+    public void openDorr()
+    {
+        if (BoolDoor == false)
+        {
+        anim.SetBool("open",true);
+        BoolDoor = anim.GetBool("open");
+            print(BoolDoor);
+        }
+           
+
+        else
+        {
+            anim.SetBool("open", false);
+            BoolDoor = anim.GetBool("open");
+            print(BoolDoor);
+        }
+
+    }
+}
